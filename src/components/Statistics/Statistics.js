f88d03d5e50ff1ffdd77c1b85ff7a7f0e9e6dd01
Vladimir Viazankin <vyazan41k@gmail.com>
2021-01-15 21:44:50 +0200

    res_hw-01

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,35 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Statistics = ({ title, stats }) => {
-  if (title === "") {
-    return (
-      <section className="statistics">
-        <ul className="stat-list">
-          {stats.map((i) => (
-            <li className="item" key={i.id}>
-              <span className="label">{i.label}</span>
-              <span className="percentage">{i.percentage}</span>
-            </li>
-          ))}
-        </ul>
-      </section>
-    );
-  } else {
-    return (
-      <section className="statistics">
-        <h2 className="title">{title}</h2>
-        <ul className="stat-list">
-          {stats.map((i) => (
-            <li className="item" key={i.id}>
-              <span className="label">{i.label}</span>
-              <span className="percentage">{i.percentage}</span>
-            </li>
-          ))}
-        </ul>
-      </section>
-    );
-  }
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(i => (
+          <li className="item" key={i.id}>
+            <span className="label">{i.label}</span>
+            <span className="percentage">{i.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 Statistics.propTypes = {
